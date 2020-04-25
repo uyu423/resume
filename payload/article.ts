6638eab24777c2945014a6d11b9f251e47436822
Yowu <me@yowu.dev>
2020-04-25 14:06:27 +0900

    2020.04.25 업데이트 (Atricle 추가)

--- payload/article.ts
+++ payload/article.ts
@@ -5,19 +5,69 @@ const article: IArticle.Payload = {
 
   list: [
     {
-      content: "IU's agency announces official online merchandise shop for fans (2020.04.22)",
-      href:
-        'https://www.gmanetwork.com/entertainment/celebritylife/news/62342/ius-agency-announces-official-online-merchandise-shop-for-fans/story',
+      content: '(2020.04.19) 우분투 20.04 LTS (Focal Fossa) 데스크탑 달라진 점과 간단 후기',
+      href: 'https://luckyyowu.tistory.com/408',
     },
     {
-      content: 'K-pop star IU donates ₩100 million to doctors for coronavirus relief (2020.02.27)',
-      href: 'http://www.koreabiomed.com/news/articleView.html?idxno=7550',
+      content: '(2020.01.02) 조금 더 안전한 랜덤 숫자(의사 난수) 생성할려고 했던 얘기',
+      href: 'https://luckyyowu.tistory.com/406',
     },
     {
-      content: 'IU donates 100 million KRW after being inspired by her fans (2019.12.30)',
-      href:
-        'https://www.allkpop.com/article/2019/12/iu-donates-to-100-million-krw-after-being-inspired-by-her-fans',
+      content: '(2019.07.29) 흔한 2019년 상반기 회고',
+      href: 'https://luckyyowu.tistory.com/404',
     },
+    {
+      content: '(2019.03.18) 흔한 개발자가 두서 없이 쓰는 글 - 서른 코 앞 흔한 개발자의 발자취',
+      href: 'https://luckyyowu.tistory.com/393',
+    },
+    {
+      content: '(2018.02.20) 28세 요우의 개발자 이직 대탐험',
+      href: 'https://luckyyowu.tistory.com/382',
+    },
+    {
+      content: '(2018.01.10) 흔한 개발자의 2017년 회고',
+      href: 'https://luckyyowu.tistory.com/380',
+    },
+    {
+      content: "(2017.06.14) '스타트업 돈보고 하냐?'",
+      href: 'https://luckyyowu.tistory.com/378',
+    },
+    {
+      content: '(2017.06.12) HTTP 404 Status Code 에 대한 고찰',
+      href: 'https://luckyyowu.tistory.com/377',
+    },
+    {
+      content: '(2017.05.29) Node.js 에서 사용할 수 있는 Repository Pattern에 대한 고민',
+      href: 'https://luckyyowu.tistory.com/376',
+    },
+    {
+      content: '(2017.05.25) 플레이팅 개발자 문제 이벤트 회고 및 해설',
+      href: 'https://luckyyowu.tistory.com/375',
+    },
+    {
+      content: '(2017.03.27) 기술 부채에 대해 (Technical Debt)',
+      href: 'https://luckyyowu.tistory.com/373',
+    },
+    {
+      content: '(2017.03.13) 소프트웨어 개발과 가치에 대해',
+      href: 'https://luckyyowu.tistory.com/372',
+    },
+    {
+      content: '(2017.03.02) 어느 스타트업의 애자일 스크럼와 JIRA에 대한 연구 문서',
+      href: 'https://luckyyowu.tistory.com/370',
+    },
+    {
+      content: '(2016.12.01) 소프트웨어 개발과 프로그래밍, 코딩이 뭐가 다르죠?',
+      href: 'https://luckyyowu.tistory.com/362',
+    },
+    {
+      content: '(2016.09.20) 졸업과 스타트업 취업에 대한 회고',
+      href: 'https://luckyyowu.tistory.com/349',
+    },
+    // {
+    //   content: '',
+    //   href: '',
+    // },
   ],
 };
 
