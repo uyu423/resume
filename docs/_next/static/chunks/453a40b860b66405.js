db5cd969b71e565db0020aa75b13e2f8997bf011
Yongwoo Yu (Yowu) <me@yowu.dev>
2026-02-17 05:08:40 +0900

    clean: docs

--- docs/_next/static/chunks/453a40b860b66405.js
+++
@@ -1,7 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/26502775900c3cf1.js",
-  "static/chunks/9fe9cedeed614f59.js",
-  "static/chunks/d43ab978ac5b76ce.css",
-  "static/chunks/eb00a0ed9ecd8fbc.css",
-  "static/chunks/turbopack-73a96ef165a8d636.js"
-])
--- docs/_next/static/chunks/b1ddee90b85c2bfc.js
+++
@@ -1,6 +0,0 @@
-__turbopack_load_page_chunks__("/", [
-  "static/chunks/02c5b5c0e4df482f.js",
-  "static/chunks/e043b0cbc2d2fc1b.js",
-  "static/chunks/9fe9cedeed614f59.js",
-  "static/chunks/turbopack-68730ad6f70c2991.js"
-])
